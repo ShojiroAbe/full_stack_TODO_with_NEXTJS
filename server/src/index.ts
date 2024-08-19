df5a3b904f190c7611532449f78760ceaac75a07
ShojiroAbe <astook89@gmail.com>
2024-08-20 00:44:24 +0900

    Todoを作成（post）するAPIを作成

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import { PrismaClient } from '@prisma/client'
 const app: Express = express();
 const PORT = 8080;
 
+app.use(express.json());
 const prisma = new PrismaClient();
 
 app.get("/allTodos", async (req: Request, res: Response) => {
@@ -13,6 +14,26 @@ app.get("/allTodos", async (req: Request, res: Response) => {
   // prisma.todo の todo は、Prisma のクライアントが schema.prisma ファイルで定義された Todo モデルを参照しています。
   const allTodos = await prisma.todo.findMany();
   return res.json(allTodos)
+  
+});
+
+app.post("/createTodo", async (req: Request, res: Response) => {
+  console.log(req.body);
+  
+  try {
+    const { title, isCompleted } = req.body;
+    const createTodo = await prisma.todo.create({
+      data: {
+        title,
+        isCompleted,
+      }
+    });
+    return res.json(createTodo)
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+
   
 });
 
@@ -31,3 +52,9 @@ app.listen(PORT, () => console.log("server is running"));
 // res オブジェクトの役割: res は、サーバーがクライアントに対して返すべきデータやステータスコードを設定するために使用されます。例えば、res.send() や res.json() を使ってデータを送信したり、res.status() でステータスコードを設定したりします。
 
 // res.json() メソッド: res.json(data) は、渡された data を JSON 形式にシリアライズし、それを HTTP レスポンスの本文としてクライアントに送信します。ここでの data は通常、JavaScript オブジェクトや配列で、JSON 形式に変換されてクライアントに返されます。
+
+// app.use(express.json()); を宣言することで、すべてのリクエストのボディが JSON フォーマットであると自動的に認識され、パースされます。これにより、リクエストボディのデータが req.body に自動的に JavaScript のオブジェクトとして格納されるため、個々のルートハンドラで JSON パースを手動で行う必要がなくなります。
+// クライアントが JSON 形式で送信した title と isCompleted のデータを req.body から直接取得しています。
+// app.use(express.json()); を使用しているので、リクエストボディが自動的にパースされ、req.body にはすでにオブジェクトとしてデータが格納されています。
+// もし app.use(express.json()); がなかった場合、req.body は undefined になり、JSON データを処理することができません。
+// app.use(express.json()); を使うことで、アプリケーション全体で JSON リクエストボディのパースが簡単に行えるようになります。特定のルートで個別に JSON パースを行う必要がないため、非常に便利で効率的
